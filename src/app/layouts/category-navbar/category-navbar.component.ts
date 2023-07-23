2ec7af93edea9cc0593a4d180fc79b4acd5f3b4d
Leo <volwahsenl@gmail.com>
2023-07-23 20:21:22 +0200

    bootsrap benutzt für navigations elemente

--- src/app/layouts/category-navbar/category-navbar.component.ts
+++ src/app/layouts/category-navbar/category-navbar.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-category-navbar',
   templateUrl: './category-navbar.component.html',
   styleUrls: ['./category-navbar.component.css']
 })
-export class CategoryNavbarComponent {
+export class CategoryNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
 
 }
